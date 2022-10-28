d6040e1be1ccbeff3e16888ba57845f992288a64
oumeima-elgharbi <oumeima.elgharbi@gmail.com>
2022-10-29 01:46:31 +0200

    best without tuning SVR, RFR more than 2h

--- surge_prediction_metric.py
+++ surge_prediction_metric.py
@@ -2,16 +2,19 @@
 
 import numpy as np
 
+
 def surge_prediction_metric(dataframe_y_true, dataframe_y_pred):
     weights = np.linspace(1, 0.1, 10)[np.newaxis]
     surge1_columns = [
         'surge1_t0', 'surge1_t1', 'surge1_t2', 'surge1_t3', 'surge1_t4',
-        'surge1_t5', 'surge1_t6', 'surge1_t7', 'surge1_t8', 'surge1_t9' ]
+        'surge1_t5', 'surge1_t6', 'surge1_t7', 'surge1_t8', 'surge1_t9']
     surge2_columns = [
         'surge2_t0', 'surge2_t1', 'surge2_t2', 'surge2_t3', 'surge2_t4',
-        'surge2_t5', 'surge2_t6', 'surge2_t7', 'surge2_t8', 'surge2_t9' ]
-    surge1_score = (weights * (dataframe_y_true[surge1_columns].values - dataframe_y_pred[surge1_columns].values)**2).mean()
-    surge2_score = (weights * (dataframe_y_true[surge2_columns].values - dataframe_y_pred[surge2_columns].values)**2).mean()
+        'surge2_t5', 'surge2_t6', 'surge2_t7', 'surge2_t8', 'surge2_t9']
+    surge1_score = (weights * (
+                dataframe_y_true[surge1_columns].values - dataframe_y_pred[surge1_columns].values) ** 2).mean()
+    surge2_score = (weights * (
+                dataframe_y_true[surge2_columns].values - dataframe_y_pred[surge2_columns].values) ** 2).mean()
 
     return surge1_score + surge2_score
 
@@ -20,10 +23,11 @@ def surge_prediction_metric(dataframe_y_true, dataframe_y_pred):
 if __name__ == '__main__':
     path_output = "./dataset/output/"
     import pandas as pd
-    CSV_FILE_Y_TRUE = path_output + 'Y_test_true.csv'  # path of the y_true csv file
-    CSV_FILE_Y_PRED = path_output + 'Y_test_pred_XGBoost.csv'  # path of the y_pred csv file
-    #CSV_FILE_Y_TRUE = 'Y_train.csv'  # path of the y_true csv file
-    #CSV_FILE_Y_PRED = 'Y_train0.csv'  # path of the y_pred csv file
+
+    CSV_FILE_Y_TRUE = path_output + 'Y_val_true.csv'  # path of the y_true csv file
+    CSV_FILE_Y_PRED = path_output + 'Y_val_pred_RFR.csv'  # path of the y_pred csv file
+    # CSV_FILE_Y_TRUE = 'Y_train.csv'  # path of the y_true csv file
+    # CSV_FILE_Y_PRED = 'Y_train0.csv'  # path of the y_pred csv file
     df_y_true = pd.read_csv(CSV_FILE_Y_TRUE, index_col=0, sep=',')
     df_y_pred = pd.read_csv(CSV_FILE_Y_PRED, index_col=0, sep=',')
     df_y_pred = df_y_pred.loc[df_y_true.index]
